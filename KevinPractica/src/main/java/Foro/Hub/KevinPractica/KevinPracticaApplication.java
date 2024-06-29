a69650c9da54d39d495d9a0a2b1ba8fb4d2e0202
Kevin Roldan <134951931+KevinRolcer@users.noreply.github.com>
2024-06-29 09:20:36 -0600

    Add files via upload

---
+++ KevinPractica/src/main/java/Foro/Hub/KevinPractica/KevinPracticaApplication.java
@@ -0,0 +1,13 @@
+package Foro.Hub.KevinPractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinPracticaApplication.class, args);
+	}
+
+}
---
+++ KevinPractica/src/test/java/Foro/Hub/KevinPractica/KevinPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package Foro.Hub.KevinPractica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
